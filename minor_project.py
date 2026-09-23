3563f2e5317bf692fb04d4ec9ac4017744e006d3
Jatin-jay-31 <jatinjaisharma916@gmail.com>
2026-09-23 12:16:51 +0530

    Some minor changes

--- minor_project.py
+++ minor_project.py
@@ -1,24 +1,29 @@
 import openpyxl
 import math
 
-def TPI(filename,sheet_name,column):
+def Average_col(filename,sheet_name,column):
     wb=openpyxl.load_workbook(filename,data_only=True)
     ws=wb[sheet_name]
     values=[]
-    for row in range(5,ws.max_row+1):
+    for row in range(6,ws.max_row+1):
         value=ws.cell(row,column).value
         if value is not None:
             values.append(value)
         print(values)
-    return math.sum(values)/len(values)
-col_tpi= TPI("12626455.xlxs","Daily Log",5)
+    return (math.sum(values))/len(values)
+col_tpi= Average_col("12626455.xlsx","Daily Log",5)
+col_phai= Average_col("12626455.xlsx","Daily Log",3)
+col_sri= Average_col("12626455.xlsx","Daily Log",2)
+col_abi= Average_col("12626455.xlsx","Daily Log",10)
+col_tui= Average_col("12626455.xlsx","Daily Log",9)
+col_dci= Average_col("12626455.xlsx","Daily Log",2)
 
 def AAI(filename,sheet_name,column1,column2):
     wb=openpyxl.load_workbook(filename,data_only=True)
     ws=wb[sheet_name]
     values1=[]
     values2=[]
-    for row in range(5,ws.max_row+1):
+    for row in range(6,ws.max_row+1):
         value1=ws.cell(row,column1).value
         value2=ws.cell(row,column2).value
         if value1 is not None:
@@ -26,58 +31,8 @@ def AAI(filename,sheet_name,column1,column2):
         if value2 is not None:
             values2.append(value2)
         print(values1)
-    return math.sum(values1)+math.sum(values2)/len(values1)
-col_aai= AAI("12626455.xlxs","Daily Log",4,6)
-
-def PhAI(filename,sheet_name,column):
-    wb=openpyxl.load_workbook(filename,data_only=True)
-    ws=wb[sheet_name]
-    values=[]
-    for row in range(5,ws.max_row+1):
-        value=ws.cell(row,column).value
-        if value is not None:
-            values.append(value)
-        print(values)
-    return math.sum(values)/len(values)
-col_phai= PhAI("12626455.xlxs","Daily Log",3)
-
-def SRI(filename,sheet_name,column):
-    wb=openpyxl.load_workbook(filename,data_only=True)
-    ws=wb[sheet_name]
-    values=[]
-    for row in range(5,ws.max_row+1):
-        value=ws.cell(row,column).value
-        if value is not None:
-            values.append(value)
-        print(values)
-    return math.sum(values)/len(values)
-col_sri= SRI("12626455.xlxs","Daily Log",2)
-
-def ABI(filename,sheet_name,column):
-    wb=openpyxl.load_workbook(filename,data_only=True)
-    ws=wb[sheet_name]
-    values=[]
-    for row in range(5,ws.max_row+1):
-        value=ws.cell(row,column).value
-        if value is not None:
-            values.append(value)
-        print(values)
-    return math.sum(values)/len(values)
-col_abi= ABI("12626455.xlxs","Daily Log",10)
-
-def TUI(filename,sheet_name,column):
-    wb=openpyxl.load_workbook(filename,data_only=True)
-    ws=wb[sheet_name]
-    values=[]
-    for row in range(5,ws.max_row+1):
-        value=ws.cell(row,column).value
-        if value is not None:
-            values.append(value)
-        print(values)
-    return math.sum(values)/len(values)
-col_tui= TUI("12626455.xlxs","Daily Log",9)
-
-    
+    return (math.sum(values1)+math.sum(values2))/len(values1)
+col_aai= AAI("12626455.xlsx","Daily Log",4,6)
 
 def EI(filename,sheet_name,col1,col2,col3):
     wb=openpyxl.load_workbook(filename,data_only=True)
@@ -88,7 +43,7 @@ def EI(filename,sheet_name,col1,col2,col3):
     lst1=[]
     lst2=[]
     lst3=[]
-    for row in range(5,ws.max_row+1):
+    for row in range(6,ws.max_row+1):
         value1=ws.cell(row,lst1).value
         value2=ws.cell(row,lst2).value
         value3=ws.cell(row,lst3).value
@@ -99,19 +54,7 @@ def EI(filename,sheet_name,col1,col2,col3):
         if value3 in day_feeling:
             lst3.append(day_feeling[value3])
     return (math.sum(lst1)+ math.sum(lst1)+math.sum(lst1))/(3 *len(lst1))
-col_ei= EI("12626455.xlxs","Daily Log",11,12,13)
-
-def DCI(filename,sheet_name,column):
-    wb=openpyxl.load_workbook(filename,data_only=True)
-    ws=wb[sheet_name]
-    values=[]
-    for row in range(5,ws.max_row+1):
-        value=ws.cell(row,column).value
-        if value is not None:
-            values.append(value)
-        print(values)
-    return math.sum(values)/len(values)
-col_dci= DCI("12626455.xlxs","Daily Log",2)
+col_ei= EI("12626455.xlsx","Daily Log",11,12,13)
 
 def PAI(a,b,c,d,e,f,g):
     pai_value=(0.15*a + 0.20*b + 0.15*c + 0.20*d + 0.15*e+ 0.10*f + 0.05*g)
